5151ffede1e60febd1cd72822c56652e64d275ac
Mostafa Azeem <mostafa.azeem91@gmail.com>
2020-06-01 23:07:11 +0200

    fix: add default selected option

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -7,10 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
     <script src="https://code.jquery.com/jquery-git.min.js"></script>
-    <!-- STYLES -->
 </head>
 <body>
 <select id="root" onchange="ajaxfunction(this)">
+<option selected disabled>Please select category</option>
 <?php foreach ($categories as $category) :?>
 <option value="<?= $category['id'] ?>"><?= $category['title'] ?></option>
 <?php endforeach;?>
@@ -25,11 +25,11 @@
         $(parent).nextAll('select').remove();
 
         var sel = $('<select>').insertAfter($(parent));
+        sel.append($('<option>').attr('selected','selected').attr('disabled','selected').text('Please select sub category'));
 
         $.ajax({
             url: 'home/createSubs/' + value.val(),
             success: function(data) {
-                console.log(data);
                 data.forEach(function(item) {
                     sel.append($('<option>').attr("value",item["id"]).text(item["title"]));
                 });
